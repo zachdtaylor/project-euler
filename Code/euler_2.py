621779744a83fcf6e929d37e08f5de47a47ed56b
Zach Taylor <zachtylr04@gmail.com>
2019-04-18 18:58:28 -0600

    Add problem 2 file

---
+++ Code/euler_2.py
@@ -0,0 +1,25 @@
+# Project Euler - Problem 2
+# Zach Taylor
+
+import math
+import time
+
+"""Calculate the sum of all even Fibonacci numbers less than 4 million. 
+
+Details
+-------
+
+Complexity
+----------
+
+"""
+
+print("Euler problem 2")
+print("Calculating solution...")
+
+start = time.time()
+solution = "no solution yet"
+end = time.time()
+
+print("Finished in {0:.7f} seconds".format(end - start))
+print("Solution:", solution)
