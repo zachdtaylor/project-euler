98fd0e85d49b4e6d996d11938869fbcb25e1f4c5
Zach Taylor <zachtylr04@gmail.com>
2020-01-09 20:11:26 -0600

    Change variable names and accept input for problem 1

--- Code/euler_1.py
+++ Code/euler_1.py
@@ -21,15 +21,17 @@ This algorithm is O(1) in both temporal and spatial complexity.
 """
 
 print("Euler problem 1")
+n = input("n=")
 print("Calculating solution...")
 
 start = time.time()
+max_lt_n = n-1
 # Numbers that evenly divide the greatest multiple of 3, 5, and 15
 # less than 1000, respectively.
-n, m, p = math.floor(1000/3), math.floor(1000/5), math.floor(1000/15)
+a, b, c = math.floor(max_lt_n/3), math.floor(max_lt_n/5), math.floor(max_lt_n/15)
 # Use formula for sum, and subtract sum of multiples of 15, since those
 # were counted twice
-solution = int((3*n*(n+1) + 5*m*(m+1) - 15*p*(p+1))/2)
+solution = int((3*a*(a+1) + 5*b*(b+1) - 15*c*(c+1))/2)
 end = time.time()
 
 print("Finished in {0:.7f} seconds".format(end - start))
